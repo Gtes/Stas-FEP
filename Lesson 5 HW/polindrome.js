1355fb2c6cf76fddfa56f5758600a564388bacfb
Gtes <gates1993@gmail.com>
2019-08-15 14:02:00 +0300

    removed  check for the first and last char

--- Lesson 5 HW/polindrome.js
+++ Lesson 5 HW/polindrome.js
@@ -4,25 +4,15 @@ const userString = 'zakaz';
 
 function isPalindrom(str) {
     let i;
-
-    /* check first and last char*/
-    if (str[0] == str[str.length - 1]) {
-        
-        /* string processing */
-        for (i = 0; i < str.length; i++) {
-            if (str[i] != str[str.length - (i + 1)]) {
-                return false;
-            }
+    /* string processing */
+    for (i = 0; i < str.length; i++) {
+        if (str[i] != str[str.length - (i + 1)]) {
+            return false;
         }
-        return true;
-    } else {
-
-        /* first last result */
-        return false;
     }
+    return true;
 };
 
 let result = isPalindrom(userString);
 
 console.log('\n' + userString + ' isPalindrom - ' + result);
-
