5d310219cfcadd5185b16cd8ae5194786597e936
Gtes <gates1993@gmail.com>
2019-09-07 16:31:58 +0300

    refactor

--- Lesson 10 HW/script.js
+++ Lesson 10 HW/script.js
@@ -1,5 +1,17 @@
+const calculator = createCalculator(10); 
+
+console.log('возвращает 55 ' + calculator.add(45));
+console.log('возвращает 10 ' + calculator.sub(45));
+console.log('возвращает 2 ' + calculator.divide(5));
+console.log('возвращает 10 ' + calculator.mult(5));
+console.log('устанавливает базовое значение в 100 ' + calculator.set(100));
+console.log('//возвращает 500 ' + calculator.mult(5));
+
+
 function createCalculator(val) {
+    /* save initial value to result*/
     let result = val;
+    
     return {
         add: (val) => result += val,
         sub: (val) => result -= val,
@@ -9,12 +21,3 @@ function createCalculator(val) {
         reset: () => result = 0
     }
 }
-
-const calculator = createCalculator(10); 
-
-console.log('// возвращает 55 ' + calculator.add(45));
-console.log('// возвращает 10 ' + calculator.sub(45));
-console.log('// возвращает 2 ' + calculator.divide(5));
-console.log('// возвращает 10 ' + calculator.mult(5));
-console.log('// устанавливает базовое значение в 100 ' + calculator.set(100));
-console.log('// возвращает 500 ' + calculator.mult(5));
