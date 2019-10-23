f173235dcb292dec155a5f97c352951ce0981ef3
Gtes <gates1993@gmail.com>
2019-10-24 01:06:22 +0300

    20 hw finished main logic

--- Lesson 20 HW/script.js
+++ Lesson 20 HW/script.js
@@ -1,47 +1,29 @@
 const BOARD_DESCRIPTION_CLASS = 'board-note-description';
 const DELETE_NOTE_BUTTON = 'delete-note-btn'
 
-
-let notesItems = [{
-        id: 1,
-        description: 'do something description'
-    },
-    {
-        id: 2,
-        description: 'do something description'
-    },
-    {
-        id: 3,
-        description: 'do something description'
-    },
-    {
-        id: 4,
-        description: 'do something description'
-    }
-];
-
 const boardNoteTemplate = document.getElementById('board-note-template').innerHTML;
 const boardContainer = document.getElementById('board');
 
 const addNoteBtn = document.querySelector('.add-note-btn');
 
 boardContainer.addEventListener('click', onBoardEvent);
+addNoteBtn.addEventListener('click', createNewEmptyNote)
 
+let listOfNotes = [];
 
-addNoteBtn.addEventListener('click', createEmptyNote)
-
-
+init();
 
 function init() {
-    notesItems = getState();
-    renderBoard(notesItems)
+    listOfNotes = getState();
+    renderBoard(listOfNotes)
 
 }
+
 
 function saveState() {
-    localStorage.setItem('board', JSON.stringify(notesItems));
-    // console.log(localStorage.getItem('board'));
+    localStorage.setItem('board', JSON.stringify(listOfNotes));
 }
+
 
 function getState() {
     const data = localStorage.getItem('board');
@@ -53,16 +35,14 @@ function getState() {
 function onNoteChange(el) {
     const id = el.parentNode.dataset.noteId
 
-
-    notesItems.find(x => x.id == id).description = el.value;
+    listOfNotes.find(x => x.id == id).description = el.value;
 
     saveState();
 }
 
+
 function onBoardEvent(e) {
-    
     const targetParentId = e.target.parentNode.dataset.noteId;        
-    
 
     switch (true) {
         case e.target.classList.contains(BOARD_DESCRIPTION_CLASS):
@@ -72,14 +52,14 @@ function onBoardEvent(e) {
             break;
 
         case e.target.classList.contains(DELETE_NOTE_BUTTON):
-                deleteNote(targetParentId)
+                deleteNote(targetParentId);
             break;
     }
 }
 
 
 function deleteNote(noteId){
-    notesItems = notesItems.filter(el => el.id != noteId);
+    listOfNotes = listOfNotes.filter(el => el.id != noteId);
 
     deleteNoteItem(noteId);
 
@@ -89,7 +69,6 @@ function deleteNote(noteId){
 
 function deleteNoteItem(noteId){
     const element = getNoteById(noteId);
-
     element && element.remove(); 
 }
 
@@ -99,14 +78,14 @@ function getNoteById(noteId){
 }
 
 
-function createEmptyNote() {
+function createNewEmptyNote() {
     const newEmptyNote = {
         id: Date.now(),
         description: ''
     }
 
     boardContainer.insertAdjacentHTML('beforeend', generateNote(newEmptyNote));
-    notesItems.push(newEmptyNote);
+    listOfNotes.push(newEmptyNote);
 
     saveState();
 }
@@ -121,50 +100,6 @@ function generateNote(note) {
 
 function renderBoard(data) {
     const boardItemsHtml = data.map((data) => generateNote(data));
-
     boardContainer.innerHTML = boardItemsHtml.join('');
 }
 
-init();
-
-// renderBoard(notesItems)
-
-// console.log(generateNote(notesItems))
-
-
-
-
-
-
-
-
-
-
-
-// function submitTodoItem() {
-//     createTodoItem(newTotoItemInput.value);
-
-//     newTodoListFormSubmit.reset();
-// }
-
-// function createNoteItem(note) {
-//     const newNote = {
-//         id: Date.now(),
-//         titile,
-//         description
-
-//     }
-//     addNoteItem(newNote);
-// }
-
-// function addNoteItem(note){
-//     boardNoteTemplate.push(note);
-
-//     addNoteItemElement(note);
-// }
-
-// function addNoteItemElement(note){
-//     const todoItemHtml = generateNote(note);
-
-//     boardContainer.insertAdjacentHTML()
-// }
