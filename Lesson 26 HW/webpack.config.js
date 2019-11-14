1593f312651122c4d5e5129227838dba5f91e96d
Gtes <gates1993@gmail.com>
2019-11-14 23:15:58 +0200

    added added htm generator

--- Lesson 26 HW/webpack.config.js
+++ Lesson 26 HW/webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     mode: 'development',
@@ -39,12 +40,17 @@ module.exports = {
                         }]
                     }
                 }
-            }
+            },
         ]
     },
     plugins: [
         new MiniCssExtractPlugin({
             filename: "style.css"
+        }),
+        new HtmlWebpackPlugin({
+            hash: true,
+            template: 'src/index.html',
+            filename: 'index.html' 
         })
     ],
 
