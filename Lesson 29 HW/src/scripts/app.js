d9a303af5d9ff02818e6c61a8ebab627505fd3cf
Gtes <gates1993@gmail.com>
2019-11-23 19:44:04 +0200

    lesson 29 HW finished

---
+++ Lesson 29 HW/src/scripts/app.js
@@ -0,0 +1,31 @@
+import Chat from "./Chats";
+import $ from 'jquery';
+
+
+const talk= new Chat({
+    onMessage: addLog,
+})
+
+
+const $log = $('#log');
+const $input = $('#message');
+const name = 'Boris';
+
+
+$('#sendBtn').on('click', sendMessage);
+
+
+function addLog(message) {
+    $log.append(
+        `<div class="${message.type}">${message.name}: ${message.message}</div>`
+    );
+    console.log('hahahahahah')
+}
+
+function sendMessage() {
+    const message = $input.val();
+    // talk.send(name, 'message', message);
+    talk.message(name, message )
+}
+
+talk.onopen(name);
